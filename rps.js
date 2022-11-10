856b3adb661ce33f66716ecd0fec0ecda3e9bc16
Sdev0212 <seandevine0212@gmail.com>
2022-11-10 15:39:31 +0000

    setup getPlayerChoice and playRound (for rock input only)

--- rps.js
+++ rps.js
@@ -1,5 +1,11 @@
 let computerChoice = "";
 
+let result;
+
+let action;
+
+let winner;
+
 function getComputerChoice() {
     let rndNumber = Math.floor(Math.random()*3); //generates a random integer from 0 to 2 (0, 1 or 2)
     switch(rndNumber) {                          //changes the random integer into a string
@@ -15,8 +21,39 @@ function getComputerChoice() {
     return computerChoice
 }
 
-function playRound(playerChoice, computerChoice) {
-    
+function getPlayerChoice() {
+    let playerChoiceRaw = prompt("please type rock paper or scissors");
+    if(playerChoiceRaw==null){
+        playerChoiceRaw = "invalid";
+    }
+    let playerChoice = playerChoiceRaw.toLowerCase();
+    return playerChoice;
 }
 
-console.log(getComputerChoice())
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if(playerChoice!="rock"&&playerChoice!="paper"&&playerChoice!="scissors") {
+        result = "You lose!";
+        action = "Didn't pick a valid option";
+        winner = "computer";
+        return [result, action, winner];
+    } else if(playerChoice=="rock"&&computerChoice=="Scissors"){
+        result = "You win!";
+        action = "Rock beats Scissors";
+        winner = "player";
+        return [result, action, winner];
+    } else if(playerChoice=="rock"&&computerChoice=="Rock"){
+        result = "Draw!";
+        action = "Same choice";
+        winner = "player";
+        return [result, action, winner];
+    }  else if(playerChoice=="rock"&&computerChoice=="Paper"){
+        result = "You lose!";
+        action = "Paper beats Rock";
+        winner = "player";
+        return [result, action, winner];
+    }
+}
+
+console.log(playRound())
